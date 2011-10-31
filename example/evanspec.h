b7efc9f07a181a708f6c46a29ebd5a58f932fbc4
Evan Hahn <me@evanhahn.com>
2011-10-31 16:18:21 -0400

    Update example EvanSpec.h

--- example/evanspec.h
+++ example/evanspec.h
@@ -3,20 +3,25 @@
 
 	EvanSpec aims to be a really simple unit testing framework for C++. It is
 	missing a number of features, but it was written to test my programming
-	projects at the University of Michigan.
+	projects at the University of Michigan and that's all.
 	
 	Enjoy!
 
-	EvanSpec is freely licensed for any use, subject to no conditions.	*/
+	Licensing info can be found in LICENSE.txt.	*/
 
 #include <iostream>
 #include <string>
+
+// Config variables
+bool PRINT_SUCCESS = true;
 
 // Expect equality, no message
 template <typename A, typename B>
 bool EXPECT_EQUALITY(A a, B b) {
 	if (a == b) {
-		std::cout << "Success: " << a << " equals " << b << "." << std::endl;
+		if (PRINT_SUCCESS) {
+			std::cout << "Success: " << a << " equals " << b << "." << std::endl;
+		}
 		return true;
 	} else {
 		std::cout << "** FAILURE: expected " << a << " to equal " << b << ". **" << std::endl;
@@ -28,7 +33,9 @@ bool EXPECT_EQUALITY(A a, B b) {
 template <typename A, typename B>
 bool EXPECT_EQUALITY(A a, B b, std::string message) {
 	if (a == b) {
-		std::cout << "Success: expected " << message << ". " << a << " equals " << b << "." << std::endl;
+		if (PRINT_SUCCESS) {
+			std::cout << "Success: expected " << message << ". " << a << " equals " << b << "." << std::endl;
+		}
 		return true;
 	} else {
 		std::cout << "** FAILURE: expected " << message << ". Expected " << a << " to equal " << b << ". **" << std::endl;
